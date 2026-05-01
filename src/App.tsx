9927c84ce43ba0ea54ebeba6c5c6ae8606c92beb
joaoquino-dev <joaoaquino.dev@outlook.com>
2026-05-01 11:49:30 -0300

    feat: calculate financial totals and integrate transaction components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
 import { useState } from "react";
 import type { Transaction } from "./types/types";
 import TransactionForm from "./components/TransactionForm";
+import TransactionList from "./components/TransactionList";
 
 function App() {
   const [incomes, setIncomes] = useState<Transaction[]>([]);
   const [expenses, setExpenses] = useState<Transaction[]>([]);
 
+  const totalIncomes = incomes.reduce((acc, curr) => {
+    return acc + curr.value;
+  }, 0);
+  const totalExpenses = expenses.reduce((acc, curr) => {
+    return acc + curr.value;
+  }, 0);
+  const balance = totalIncomes - totalExpenses;
+
+  const handleAddIncome = (name: string, value: number) => {
+    const newTransaction: Transaction = {
+      id: crypto.randomUUID(),
+      name,
+      value,
+    };
+    setIncomes([...incomes, newTransaction]);
+  };
+
+  const handleAddExpense = (name: string, value: number) => {
+    const newTransaction: Transaction = {
+      id: crypto.randomUUID(),
+      name,
+      value,
+    };
+    setExpenses([...expenses, newTransaction]);
+  };
+
   return (
     <div>
-      <TransactionForm onAdd={setIncomes} />
+      <TransactionForm onAdd={handleAddIncome} />
+      <TransactionForm onAdd={handleAddExpense} />
+      <TransactionList transactions={incomes} />
+      <TransactionList transactions={expenses} />
     </div>
   );
 }
