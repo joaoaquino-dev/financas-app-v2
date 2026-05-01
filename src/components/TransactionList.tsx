f8a4deac453a6fe90039cb51c086bc791f707176
joaoquino-dev <joaoaquino.dev@outlook.com>
2026-05-01 11:47:59 -0300

    feat: implement TransactionList to render transactions

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -1,2 +1,23 @@
+import { type Transaction } from "../types/types";
 
+type TransactionListProps = {
+  transactions: Transaction[];
+};
+const TransactionList = ({ transactions }: TransactionListProps) => {
+  return (
+    <div>
+      <div>
+        <div>
+          {transactions.map((transaction) => (
+            <div key={transaction.id}>
+              {transaction.name}
+              {transaction.value}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
+export default TransactionList;
