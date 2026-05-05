f38786c2a67fa106c40c6b0115c58fc3e4a1788e
joaoquino-dev <joaoaquino.dev@outlook.com>
2026-05-05 09:34:50 -0300

    feat: add remove button to TransactionList

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -2,16 +2,17 @@ import { type Transaction } from "../types/types";
 
 type TransactionListProps = {
   transactions: Transaction[];
+  onRemove: (id: string) => void;
 };
-const TransactionList = ({ transactions }: TransactionListProps) => {
+const TransactionList = ({ transactions, onRemove }: TransactionListProps) => {
   return (
     <div>
       <div>
         <div>
           {transactions.map((transaction) => (
             <div key={transaction.id}>
-              {transaction.name}
-              {transaction.value}
+              {transaction.name} {transaction.value}
+              <button onClick={() => onRemove(transaction.id)}>Apagar</button>
             </div>
           ))}
         </div>
